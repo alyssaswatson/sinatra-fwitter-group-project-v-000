054168aa93387ce1c8352b3100ef1ce7060be08b
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-30 03:13:52 +0000

    automatically backed up by learn

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,5 @@
 
-class UsersController < ApplicationController
+class UserController < ApplicationController
   get '/users/:slug' do
     @user = User.find_by_slug(params[:slug])
     erb :user_show
