0f2fbb81a8656bde99ce309560d57705325a8ad2
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-03-30 02:54:46 +0000

    automatically backed up by learn

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -33,7 +33,7 @@ class UserController < ApplicationController
 
 
   post '/login' do
-    #binding.pry
+    binding.pry
     user = User.find_by(:username => params[:username])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
